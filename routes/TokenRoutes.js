d28004ae9d8d698d001bba22782b5bdff52ca25d
Juri Kalita <jurikalita011@gmail.com>
2024-02-28 07:11:38 +0530

    fixed router

--- routes/TokenRoutes.js
+++ routes/TokenRoutes.js
@@ -13,19 +13,17 @@ TokenRouter.post("/add", async (req, res) => {
 });
 
 TokenRouter.get("/", async (req, res) => {
-  router.get("/tokens", async (req, res) => {
-    try {
-      const { page, limit } = req.query;
-      if (!page) page = 1;
-      const tokenData = await TokenModel.find()
-        .skip((page - 1) * 60)
-        .limit(limit);
-      res.status(200).send({ data: tokenData });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ msg: "No token data found", err: error.message });
-    }
-  });
+  try {
+    const { page, limit } = req.query;
+    if (!page) page = 1;
+    const tokenData = await TokenModel.find()
+      .skip((page - 1) * 60)
+      .limit(limit);
+    res.status(200).send({ data: tokenData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "No token data found", err: error.message });
+  }
 });
 
 module.exports = TokenRouter;
